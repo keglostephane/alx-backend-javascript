38bf40969fc41466abfdda5a065bc94f1951064c
Keglo Dodzi <stephanekeglo@gmail.com>
2024-04-22 22:59:03 +0000

    Fix 1-block-scoped.js #5

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
 
   if (trueOrFalse) {
-    var task1 = true;
-    var task3 = false;
+    var task = true;
+    var task2 = false;
   }
+
+  task = false;
+  task2 = true;
 
   return [task, task2];
 }

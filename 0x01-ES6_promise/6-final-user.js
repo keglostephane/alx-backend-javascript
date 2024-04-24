cbb7a1b8f6ab39812904be8892219776503d3a32
Keglo Dodzi <stephanekeglo@gmail.com>
2024-04-24 23:57:27 +0000

    Add 6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo]).then((results) => {
+    const resp = [];
+    results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        resp.push({ status: result.status, value: result.value });
+      } else {
+        resp.push({ status: result.status, value: result.reason });
+      }
+    });
+    return resp;
+  });
+}

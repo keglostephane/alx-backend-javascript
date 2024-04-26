f69549ae7ba9d477545fa71e685a2119a635670d
Keglo Dodzi <stephanekeglo@gmail.com>
2024-04-26 07:19:57 +0000

    Add 100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-underscore-dangle */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+    constructor(brand, motor, color, range) {
+        super(brand, color, motor);
+        this._range = range;
+    }
+
+    cloneCar() {
+        return new Car;
+    }
+}

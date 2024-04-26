4e2ddb67b8e7a8473d41fd5038eaa282f932a05f
Keglo Dodzi <stephanekeglo@gmail.com>
2024-04-26 22:43:21 +0000

    Fix #4

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,7 +2,7 @@
 export default class Building {
   constructor(sqft) {
     if (new.target !== 'Building'
-          && typeof this.evacuationWarningMessage !== 'function') {
+          && typeof this.evacuationWarningMessage === 'undefined') {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;

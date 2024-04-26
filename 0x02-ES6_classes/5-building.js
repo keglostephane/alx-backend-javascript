ebf2b7ec912f41e1c0dfc18f2317389c281a0b04
Keglo Dodzi <stephanekeglo@gmail.com>
2024-04-26 23:01:35 +0000

    Fix 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-underscore-dangle */
 export default class Building {
   constructor(sqft) {
-    if (new.target !== 'Building'
-          && typeof this.evacuationWarningMessage === 'undefined') {
+    if (new.target !== Building
+          && this.evacuationWarningMessage === undefined) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;

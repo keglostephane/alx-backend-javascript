9c84bf11fcaec76b25ebb3892027445546b991c3
Keglo Dodzi <stephanekeglo@gmail.com>
2024-04-26 03:43:18 +0000

    Add 6-sky_high.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return super.sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}

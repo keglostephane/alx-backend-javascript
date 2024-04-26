8c72d46a5d0e2da93061458d38545c8907dc59ef
Keglo Dodzi <stephanekeglo@gmail.com>
2024-04-26 04:00:36 +0000

    Add 7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}

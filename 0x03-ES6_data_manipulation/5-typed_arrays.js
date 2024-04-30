0fb7d4c1864431191519cc4cfab7a2bac95fa30e
Keglo Dodzi <stephanekeglo@gmail.com>
2024-04-30 23:06:24 +0000

    Add 5-typed_arrays.js

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new DataView(buffer);
+  int8Array.setInt8(position, value);
+
+  return int8Array;
+}

20ec96bf1c4b59da40cfb46c4cfcfe2f2e9f39a2
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-01 23:39:08 +0000

    Fix Teacher interface implementation

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,8 @@
 interface Teacher {
-    firstName: string;
-    lastName: string;
+    readonly firstName: string;
+    readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
     [key: string]: boolean | string | number;
-}
-
-interface Directors extends Teacher {
-    numberOfReports: number;
 }

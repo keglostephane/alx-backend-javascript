2da757aa1034e848ce0e3780ddf94b4d6d25b696
Keglo Dodzi <stephanekeglo@gmail.com>
2024-05-01 23:11:48 +0000

    Add Directors interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,17 @@ interface Teacher {
     location: string;
     [key: string]: boolean | string | number;
 }
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+}
+
+console.log(director1);

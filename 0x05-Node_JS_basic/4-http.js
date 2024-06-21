26bd0ef2189a7f8c9bde679ae0a370a1d4e06bc5
Keglo Dodzi <stephanekeglo@gmail.com>
2024-06-21 16:33:33 +0000

    Add 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-param-reassign */
+const http = require('http');
+
+const app = http.createServer();
+
+app
+  .on('request', (req, resp) => {
+    resp.statusCode = 200;
+    resp.write('Hello Holberton School');
+    resp.end();
+  })
+  .listen(1245);
+
+module.exports = app;

e44bb2a6f6df3bc969fc9c39d9a11a726234c061
Keglo Dodzi <stephanekeglo@gmail.com>
2024-07-22 02:30:39 +0000

    Add Task 0 files

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,15 @@
+/* eslint-env mocha */
+const assert = require('assert')
+const calculateNumber = require('./0-calcul')
+
+describe('sum of numbers', function () {
+  it('two integers', function () {
+    assert.equal(calculateNumber(2, 3), 5)
+  })
+  it('one integer, one floating point', function () {
+    assert.equal(calculateNumber(2.3, 3), 5)
+  })
+  it('two floating point', function () {
+    assert.equal(calculateNumber(2.1, 3.6), 6)
+  })
+})

b254f04acfdec25978917c61a0a699abdc2b7ea8
Keglo Dodzi <stephanekeglo@gmail.com>
2024-07-23 14:35:51 +0000

    Fix utils.js

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,5 +1,5 @@
 module.exports.Utils = {
-  calculateNumber: function (type, a, b) {
+  calculateNumber (type, a, b) {
     if (type === 'SUM') return Math.round(a) + Math.round(b)
     else if (type === 'SUBTRACT') return Math.round(a) - Math.round(b)
     else if (type === 'DIVIDE') return Math.round(b) ? Math.round(a) / Math.round(b) : 'Error'

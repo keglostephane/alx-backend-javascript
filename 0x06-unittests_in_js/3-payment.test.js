e22d7f2f63e7e5fd44f26796d1ffea31be2cf835
Keglo Dodzi <stephanekeglo@gmail.com>
2024-07-23 16:37:28 +0000

    Fix #4#

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,13 +1,13 @@
 /* eslint-env mocha */
 const { expect } = require('chai')
 const sinon = require('sinon')
-const { Utils } = require('./utils')
 const sendPaymentRequestToApi = require('./3-payment')
+const Utils = require('./utils.js')
 
 describe('sendPaymentRequestToApi', function createSpy () {
   let spy
   before(function () {
-    spy = sinon.spy(Utils, 'calculateNumber')
+    spy = sinon.spy(Utils ,'calculateNumber')
   })
   after(function RestoreSpy () {
     spy.restore()

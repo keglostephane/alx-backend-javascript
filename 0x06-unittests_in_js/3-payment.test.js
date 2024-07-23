608f40c6bd0c3aee964af521169cb0ba5325c7c6
Keglo Dodzi <stephanekeglo@gmail.com>
2024-07-23 13:06:39 +0000

    Add task 3 files

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,19 @@
+/* eslint-env mocha */
+const { expect } = require('chai')
+const sinon = require('sinon')
+const { sendPaymentRequestToApi } = require('./3-payment')
+const { Utils } = require('./utils')
+
+describe('sendPaymentRequestToApi', function createSpy () {
+  let spy
+  before(function () {
+    spy = sinon.spy(Utils, 'calculateNumber')
+  })
+  after(function RestoreSpy () {
+    spy.restore()
+  })
+  it('should call Utils.calculateNumber with SUM and correct arguments', function () {
+    sendPaymentRequestToApi(100, 20)
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.equal(true)
+  })
+})

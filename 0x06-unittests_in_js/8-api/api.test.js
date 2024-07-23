98189d7af350e3aa05f27c77ddebfefe02f7ea80
Keglo Dodzi <stephanekeglo@gmail.com>
2024-07-23 23:37:17 +0000

    Fix #2

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,18 +1,20 @@
 const { expect } = require('chai')
 const request = require('request')
-const { promisify } = require('util')
 
 const url ='http://localhost:7865'
-const requestGet = promisify(request.get)
 
 describe('api index page', function () {
 
-    it('should returns 200 status code', async function() {
-        const { statusCode } = await requestGet(url)
-        expect(statusCode).to.equal(200)
+    it('should returns 200 status code', function(done) {
+        request.get(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200)
+            done()
+        })
     })
-    it('should returns correct message', async function() {
-        const { body } = await requestGet(url)
-        expect(body).to.equal('Welcome to the payment system')
+    it('should returns correct message', function(done) {
+        request.get(url, function(error, response, body) {
+            expect(body).to.equal('Welcome to the payment system')
+            done()
+        })
     })
 })

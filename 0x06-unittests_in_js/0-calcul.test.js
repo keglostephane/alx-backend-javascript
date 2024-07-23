ac2c0af77702b0ae7dce54f2e3451c7f4160b64d
Keglo Dodzi <stephanekeglo@gmail.com>
2024-07-23 15:01:29 +0000

    Fix test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,7 +7,7 @@ describe('sum of numbers', function () {
     assert.equal(calculateNumber(2, 3), 5)
   })
   it('one integer, one floating point', function () {
-    assert.equal(calculateNumber(2.3, 3), 5)
+    assert.equal(calculateNumber(2, 3.2), 5)
   })
   it('two floating point', function () {
     assert.equal(calculateNumber(2.2, 3.6), 6)
